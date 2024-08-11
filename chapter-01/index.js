19d44d22afff56db86e234ffbf53f94df4501044
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-11 23:23:20 +0600

    Add files via upload

---
+++ chapter-01/index.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+console.log("Lets Build a Pyramid shape With a Limited vertice - 5, with javascript Basics...");
+console.log("      #    ");
+console.log("     ###   ");
+console.log("    ##### ");
+console.log("   ####### ");
+console.log("  ######### ");
+
+// this is desire to show the output of the programms..
