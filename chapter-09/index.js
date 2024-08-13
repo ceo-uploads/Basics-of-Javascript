922a4b34297ffaf01c4a2ebc30e921568c839ecc
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-13 22:45:07 +0600

    Add files via upload

---
+++ chapter-09/index.js
@@ -0,0 +1,10 @@
+let char1 = "Hello";
+console.log(char1);
+
+let char2 = "World";
+console.log(char2);
+
+let secondCharacter;
+console.log(secondCharacter); // it will show undefind..
+
+// chapter 9 - if a variable has no value,, it will show undefined.
