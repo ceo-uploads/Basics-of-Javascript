cd17aac471fae76fdf8dfb27351dfe6ddcaa2487
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-13 22:45:26 +0600

    Add files via upload

---
+++ chapter-10/index.js
@@ -0,0 +1,8 @@
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = 'Test';
+console.log(secondCharacter);
+
+// chapter 10 - defining variable..
