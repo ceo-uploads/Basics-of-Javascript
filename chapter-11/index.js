d4022cbfab446b132448ff02725c265098702f30
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-13 22:45:56 +0600

    Add files via upload

---
+++ chapter-11/index.js
@@ -0,0 +1,9 @@
+let character = "Hello";
+console.log(character);
+
+character = 'world!';
+
+let secondCharacter = character;
+console.log(secondCharacter);
+
+// chapter 11 - You can also assign the value of a variable to another variable. 
