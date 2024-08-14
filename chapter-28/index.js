b26ae37509a39e966466762fe0a45af82d1dd8af
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:04:26 +0600

    Add files via upload

---
+++ chapter-28/index.js
@@ -0,0 +1,10 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
+
+// chapter 28 - this is to view the returning element by the method .push()....
