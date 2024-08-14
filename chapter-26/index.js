6a848fc9e9b59edd07bab1a19078f50d96713c59
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:03:44 +0600

    Add files via upload

---
+++ chapter-26/index.js
@@ -0,0 +1,6 @@
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+
+// chapter 26 - this is an example of ,push() method,
+// by this,, we are able to add an existing element to the array...
