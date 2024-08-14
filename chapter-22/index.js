94a950a0c0cda12387d46846d986b06a93f18b8a
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:02:24 +0600

    Add files via upload

---
+++ chapter-22/index.js
@@ -0,0 +1,8 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+console.log(rows[0]);
+rows[rows.length - 1 ] = 10;
+
+// chapter 22 - we can assign the value of the array index with the referance of it's length...
