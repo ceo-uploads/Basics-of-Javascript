65c61224a4827d5bbdcf4c30a163b86d1ae556a7
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:03:02 +0600

    Add files via upload

---
+++ chapter-24/index.js
@@ -0,0 +1,6 @@
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
+
+// chapter 24 - was an example of previous works...
