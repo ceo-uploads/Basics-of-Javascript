f9f64b7cd2c465ce48ab2d65bdcdbd97d9c19a13
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:04:04 +0600

    Add files via upload

---
+++ chapter-27/index.js
@@ -0,0 +1,11 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+
+let popped = rows.pop();
+console.log(popped);
+
+// chapter 27 - is to define a method called .pop()..
+// by this, we will be able to remove the lastb element from the array... and prints the popped element...
