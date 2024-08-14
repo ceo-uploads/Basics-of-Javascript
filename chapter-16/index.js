077ba7df79be72babb6f48b793d01486d451ecb2
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-14 22:49:09 +0600

    Add files via upload

---
+++ chapter-16/index.js
@@ -0,0 +1,6 @@
+let character = 'Hello';
+let count;
+console.log(count + 1);
+
+// chapter 16 - this is to show that what happens if we do a mathmetical operation without 
+// declaring any value inside.  The Console will print NaN...
