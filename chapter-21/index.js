0e5d3751581782775c15c0338f7199a4e4244bd6
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-15 02:02:06 +0600

    Add files via upload

---
+++ chapter-21/index.js
@@ -0,0 +1,9 @@
+let character = 'Hello';
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows);
+rows[2] = 10;
+
+console.log(rows[2]);
+
+// chapter 21 - is used to show that, we can change the value of any 
+// index point in the array..
