e5cc0792b720c0524687ab3fa59a7f28bea78836
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-16 01:11:20 +0600

    Add files via upload

---
+++ chapter-34/index.js
@@ -0,0 +1,7 @@
+for (let i = 0; "condition"; "iteration") {
+
+}
+
+// Replace the string "iterator" with a let declaration for 
+// the variable i. Assign it the value 0 to start. This will give 
+// the i variable the value 0 the first time your loop runs.
