8f2f1604eb7a97eee8a3bf1eacf48a10d8872cc4
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-16 01:10:23 +0600

    Add files via upload

---
+++ chapter-30/index.js
@@ -0,0 +1,12 @@
+let character = "Hello";
+let count = 8;
+let rows = [];
+
+// we were ordered to do so,,
+// the Command Was..
+/*
+  Change your rows declaration to be assigned an empty array again.
+
+  Also, change your 'Hello' string to use double quotes again. Generally,
+  it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project. 
+*/
