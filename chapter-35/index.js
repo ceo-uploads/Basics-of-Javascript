b0b7fa918b056edb34af1140261391285a6a2b0f
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-16 01:11:33 +0600

    Add files via upload

---
+++ chapter-35/index.js
@@ -0,0 +1,11 @@
+let count = 8;
+for (let i = 0; i < count; "iteration") {
+
+}
+
+// For now, you will use the less than operator (<). 
+// his allows you to check if the value on the left is less than
+// the value on the right. For example, count < 3 would evaluate to
+// true if count is 2, and false if count is 4.
+
+// Replace your "condition" string with a condition to check if i is less than count.
