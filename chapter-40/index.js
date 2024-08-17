ecb828e470ae09d609eb11fa4e12d2018dc8e5d9
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-17 23:00:17 +0600

    Add files via upload

---
+++ chapter-40/index.js
@@ -0,0 +1,14 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
+
+console.log(result);
+
+// chapter 40 - Add a log statement to print the value of result.
+// Depending on which console you use, you may not see anything printed.
