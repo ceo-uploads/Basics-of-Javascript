56829ab4f5f9d14eb3611f164d46139caa5f26d5
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-17 22:59:42 +0600

    Add files via upload

---
+++ chapter-39/index.js
@@ -0,0 +1,12 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = "";
+
+// chapter 39 - Use let to declare a result variable, and assign it an empty
+// string. An empty string is represented by quotation marks with nothing between them, such as "".
