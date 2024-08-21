10c895ea2c566dad19b47c958e64f04794263f12
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-21 23:59:25 +0600

    Add files via upload

---
+++ chapter-61/index.js
@@ -0,0 +1,28 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+  const test = "Testing";
+  return test;
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 61 - Now your call variable has the value "Testing". But your function is no longer using the name parameter.
+
+// Remove the name parameter from your function declaration, then remove your "CamperChan" string from the padRow call.
+
+// Also, remove both console.log from the padRow function.
