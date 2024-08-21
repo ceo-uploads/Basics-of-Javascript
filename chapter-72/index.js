f52410403cfd951033448bba9dd29180dd512c53
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-22 02:24:13 +0600

    Add files via upload

---
+++ chapter-72/index.js
@@ -0,0 +1,21 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i++) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 72 - Replace your addition assignment with the increment operator for your loop iteration... 
