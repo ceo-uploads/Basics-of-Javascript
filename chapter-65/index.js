f3cee7365bbe3e8833ada4e720e0eb237ad52577
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-23 02:07:37 +0600

    Add files via upload

---
+++ chapter-65/index.js
@@ -0,0 +1,22 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 65 - Use the return keyword to return the value of the character variable, repeated rowNumber times.
