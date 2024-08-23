bee03fe66e43ab2b74ffed465e67872ad868285c
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-24 00:34:05 +0600

    Add files via upload

---
+++ chapter-83/index.js
@@ -0,0 +1,31 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 83 - Add an else block to the else if block. Inside the else block,
+// log the string "This is the else block" to the console.
