6ea0a248cb1374db3c122cfdec3d467c3b920854
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-24 00:34:28 +0600

    Add files via upload

---
+++ chapter-85/index.js
@@ -0,0 +1,28 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+while (continueLoop) {
+
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 85 - Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
