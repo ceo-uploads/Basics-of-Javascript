35054e747bc947b370664554e02bf821ca6800da
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-25 02:36:16 +0600

    Add files via upload

---
+++ chapter-97/index.js
@@ -0,0 +1,30 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+//let done = 0;
+
+while (rows.length < count) {
+  //done++;
+  rows.push(padRow(rows.length + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 97 - Now you no longer need your done variable.
+// Remove the increment operation from your loop, and the variable declaration for done.
