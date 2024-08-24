5f5f540bf703b3633ef178052b204729a35e0328
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-25 02:35:34 +0600

    Add files via upload

---
+++ chapter-94/index.js
@@ -0,0 +1,32 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let done = 0;
+
+while (done <= count) {
+  done++;
+  rows.push(padRow(done, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// chapter 94 - Because you are only checking if done is not equal to count,
+// if done were to be larger than count your loop would go on forever.
+
+// Update your loop's condition to check if done is less than or equal to count.
